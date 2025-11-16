f2038ecfa56c70e5355bd8b901b380fd4086c593
dopey2 <dani.gabor1996@gmail.com>
2025-11-16 12:24:04 +0100

    chore: remove 'packages/_specs' as it was moved inside the expressions package

--- packages/_specs/cucumber.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    default: [
-        `features/**/*.feature`,
-        `--require-module ts-node/register`,
-        `--require step_definitions/**/*.ts`,
-        `--format-options '{"snippetInterface": "synchronous"}'`,
-        `-f @cucumber/pretty-formatter`
-    ].join(' '),
-};
